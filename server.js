6c7036f264b54ead0567119d68e5cf694d23044a
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2021-08-25 21:06:01 -0700

    pwa configuration

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express(); // create express app
+
+app.use(express.static("www"));
+
+const port = process.env.PORT || 8080
+
+// start express server on port 8080
+app.listen(port, () => {
+  console.log("server started on port 8080");
+});
